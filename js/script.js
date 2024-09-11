a085ee0dbd1eee20ce65025ac7a17b1525a30219
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-11 15:20:51 +0200

    recuperata mail da API

--- js/script.js
+++ js/script.js
@@ -3,7 +3,22 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            email: '',
         }
+    },
+
+    methods: {
+        getRandomEmail() {
+            axios.get(this.apiUrl)
+                .then((response) => {
+                    console.log(response.data.response);
+                    this.email = response.data.response;
+                });
+        }
+    },
+
+    mounted() {
+        this.getRandomEmail();
     }
 }).mount('#app')
